6bc4510f5cd4899d39e8a2e1758981db0460afe9
Akhileshwar Padala <apadala1@kent.edu>
2018-10-01 13:19:39 -0400

    P001

--- ProjectEuler/src/main/java/com/apadala/ProjectEuler/App.java
+++ ProjectEuler/src/main/java/com/apadala/ProjectEuler/App.java
@@ -4,10 +4,15 @@ package com.apadala.ProjectEuler;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		long lStartTime = System.nanoTime();
+		System.out.println("-----------------------------");
+		P001.Solution();
+		System.out.println("-----------------------------");
+		long lEndTime = System.nanoTime();
+		long output = lEndTime - lStartTime;
+		System.out.println("\nElapsed time in milliseconds: " + output);
+	}
 }
+ 
